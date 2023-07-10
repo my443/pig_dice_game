b0daffeac7fcefda6125697f34cc2caa448020c3
John van Dijk <john@fortyfour-three.com>
2023-07-10 09:58:34 -0400

    Intermdiate point commit.

--- src/game.py
+++ src/game.py
@@ -1,13 +1,22 @@
 from player import *
+from turn import *
+import os
 
 class Game():
     players = []
     endgame = False
-    current_turn = {}
+    current_turn_player = None
+    current_turn_details = Turn()
 
     def add_player(self, name):
         new_player = Player(name)
         self.players.append(new_player)
+
+        # If you have just one person when you add a player,
+        # That is where the game starts
+        if len(self.players) == 1:
+            self.assign_current_turn()
+            self.new_turn()
 
     def remove_player(self, player_to_remove_index):
         del self.players[int(player_to_remove_index)-1]
@@ -24,18 +33,47 @@ class Game():
         return self.players
 
     def assign_current_turn(self):
-        player = 0
+        self.current_turn_player = 0
 
-        pass
+    def get_turn(self):
+        turn = self.current_turn_details
+        if len(self.players) > 0:
+            print(f'Current Player: {self.players[self.current_turn_player].name}\n')
+        print (turn)
+
+        return turn
+
+    def new_turn(self):
+        new_turn = Turn()
+
+        if self.current_turn_player + 1 > len(self.players):
+            self.current_turn_player = 0
+        else:
+            self.current_turn_player + 1
+
+        self.current_turn_details = new_turn
+
+    def roll_dice(self):
+        myturn = self.current_turn_details
+        roll = myturn.add_roll()
+        return roll
 
     def get_action(self):
-        options = { 'player_roll_dice': 'a) Current player rolls di',
+        options = { 'player_roll_dice': 'a) Current player rolls dice',
                     'player_ends_turn': 'b) Current Player stops rolling',
                     'add_player':       'c) Add player',
                     'remove_player':    'd) Remove Player',
                     'quit_game':        'e) Quit game.'}
 
+        os.system('cls')
+
         self.get_players()
+        this_turn = self.get_turn()
+        print (this_turn)
+        # if len(self.players) > 1:
+        #     if this_turn.turn_is_active() == False:
+        #         self.new_turn()
+
 
         for k, v in options.items():
             print (v)
@@ -43,8 +81,12 @@ class Game():
         selection = input('Select option: ')
 
         if selection == 'a':
-            pass
+            roll = self.roll_dice()
+            print('roll: ', roll)
+            print (self.current_turn_details.get_score())
+            input('')
         elif selection == 'b':
+            for i in range(10): print('ending turn now')
             pass
         elif selection == 'c':
            new_player_name = input("Enter New Player's name: ")
@@ -54,6 +96,7 @@ class Game():
             self.remove_player(player_to_remove)
         elif selection == 'e':
             self.endgame = True
+
         if self.endgame != True:
             self.get_action()
 
--- src/turn.py
+++ src/turn.py
@@ -1,39 +1,53 @@
 from enum import *
+import random
 
 class Turn():
     current_score = 0
     score_values = []
     TurnStatus = Enum('TurnStatus', ['Active', 'Ended'])
     curren_status = TurnStatus.Active
-    chose_to_end_turn = False
 
-    def add_roll(self, roll):
+    def add_roll(self):
+        roll = random.randint(1, 2)
+
+        if roll == 1:
+            self.end_turn('rolled a 1')
+
         if self.get_turn_status() == self.TurnStatus.Active:
             self.score_values.append(roll)
 
+        return roll
+
+
     def get_score(self):
         if self.get_turn_status() == self.TurnStatus.Active:
-            return sum(self.score_values)
+            self.current_score = sum(self.score_values)
+            return self.current_score
         else:
-            return 0
-
-    def set_turn_status(self):
-        if len(self.score_values) > 0 and self.score_values[-1] == 0 or self.chose_to_end_turn == True:
-            self.curren_status = self.TurnStatus.Ended
-        else:
-            self.curren_status = self.TurnStatus.Active
+            self.current_score = 0
+            return self.current_score
 
     def get_turn_status(self):
-        self.set_turn_status()
         return self.curren_status
 
-    def end_turn(self):
-        self.chose_to_end_turn = True
+    def turn_is_active(self):
+        return self.get_turn_status() != self.TurnStatus.Ended
+
+    def end_turn(self, reason):
+        if reason != 'player':
+            self.score_values = []
+
+        self.curren_status = self.TurnStatus.Ended
 
     def __init__(self):
         self.current_status = self.TurnStatus.Active
         self.score_values = []
-        pass
+
+    def __str__(self):
+        return f'***************************************' \
+               f'\nCurrent score: {self.get_score()}' +\
+                f'\n{self.get_turn_status()}\n' \
+                f'***************************************\n'
 
 if __name__ == '__main__':
     pass
